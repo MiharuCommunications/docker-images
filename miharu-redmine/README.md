a530723814ed804197f6dd31e0aa23f25290b99e
Hirofumi SAKODA <sako@miharu.co.jp>
2023-01-11 15:03:56 +0900

    Delete miharu-redmine

--- miharu-redmine/README.md
+++
@@ -1,5 +0,0 @@
-2022-12-27 sako
-miharu-redmine
-   using 
-   https://github.com/sameersbn/docker-redmine/tree/4.2.9
-
--- miharu-redmine/plugin_files/pre-install.sh
+++
@@ -1,8 +0,0 @@
-apt-get update
-apt-get install libmysqlclient-dev
-apt-get install libsqlite3-dev
-apt-get install libpq-dev
-
-bundle update rubocop
-bundle install --without development test
-# bundle exec rake redmine:plugins NAME=redmineup_tags RAILS_ENV=production
